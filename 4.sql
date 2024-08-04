462b1dca6243f6fbaf92cca6ce3ede6150d827b7
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2024-08-04 11:38:33 +0530

    Update 4.sql

--- 4.sql
+++ 4.sql
@@ -1,1 +1,3 @@
-SELECT a.cust_name AS "Customer Name", a.city, b.name AS "Salesman", b.commission FROM customer a INNER JOIN salesman b ON a.salesman_id=b.salesman_id WHERE b.commission>.12;
+
+SELECT a.cust_name AS "Customer Name", a.city, b.name AS "Salesman", b.commission FROM customer a INNER JOIN salesman 
+  b ON a.salesman_id=b.salesman_id WHERE b.commission>.12;
