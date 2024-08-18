3e615b7ae859b8bcc28a4e6104a4b16530d15f85
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2024-08-18 23:34:58 +0530

    Update 8.sql

--- 8.sql
+++ 8.sql
@@ -1,4 +1,5 @@
 SELECT  c.cust_name, c.city,c.grade,s.name AS "Salesman" , s.city
-From customer c inner join salesman s on  c.salesman_id = s.salesman_id
+From customer c inner join salesman s 
+  on  c.salesman_id = s.salesman_id
 ORDER BY c.customer_id;
   
